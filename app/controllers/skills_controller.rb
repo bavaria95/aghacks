7bed5eb893b8093fb0641e40e0d710b9ae261926
Piotr Jaworski <piotr.jaworski@live.com>
2014-10-24 22:20:56 +0200

    added - add new skill

---
+++ app/controllers/skills_controller.rb
@@ -0,0 +1,55 @@
+class SkillsController < ApplicationController
+
+  def index
+    @skills = Skill.all
+  end
+
+  def new
+    @skill = Skill.new
+  end
+
+  def create
+    @skill = Skill.create(skill_params)
+    if @skill.save
+      redirect_to root_path
+      flash[:notice] = "Skill has been created!"
+    else
+      redirect_to root_path
+      flash[:notice] = "Error!"
+    end
+  end
+
+  def edit
+    @skill = Skill.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @skill = Skill.find(params[:id])
+    @skill.update_attributes(skill_params)
+    if @skill.save
+      redirect_to root_path
+      flash[:notice] = "Edition ended successful!"
+    else
+      redirect_to root_path
+      flash[:alert] = "An error has accured, didn't you fill all fields?"
+    end
+  end
+
+  def destroy
+    @skill = Skill.find(params[:id])
+    @skill.destroy
+    redirect_to root_path
+    flash[:notice] = "Skill has been deleted!"
+  end
+
+  def show
+    @skill = Skill.find(params[:id])
+  end
+
+  private
+
+  def skill_params
+    params.require(:skill).permit(:skill)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
   root 'page#index'
 
+  resources :skills
+
 end
