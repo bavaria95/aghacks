b6cba1a8e1b1a9cc5aaca9c624a6d663db2f3f53
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-10-25 01:25:13 +0200

    Projects from Angular wip

--- app/assets/javascripts/controllers/mainCtrl.js
+++ app/assets/javascripts/controllers/mainCtrl.js
@@ -1,11 +1,21 @@
 (function () {
     'use strict';
 
-    function MainCtrl($scope) {
+    function MainCtrl($scope, $resource) {
         console.log(1)
+
+        var projectsFactory = $resource('/projects.json/:id', { id: '@id' });
+        var projects = projectsFactory.query();
+
+        $scope.projects = projects;
+
+        $scope.search = function(){
+            var pr = $scope.projects;
+            console.log(2)
+        }
     }
     
     angular
-        .module('app')
+        .module('app', ['ngResource'])
         .controller('MainCtrl', MainCtrl);
 })();
