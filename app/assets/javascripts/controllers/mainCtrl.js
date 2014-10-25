41aa05ffaea2d0f7e6f3e98e608df31fcd82f97c
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-10-25 02:44:29 +0200

    Users

--- app/assets/javascripts/controllers/mainCtrl.js
+++ app/assets/javascripts/controllers/mainCtrl.js
@@ -6,20 +6,37 @@
             || project.long_description.indexOf(word) > -1;
     }
 
+    function userContains(user, word) {
+        return user.username.indexOf(word) > -1;
+        // TODO Description
+            // || user.long_description.indexOf(word) > -1;
+    }
+
     function MainCtrl($resource) {
         var vm = this;
 
         var projectsFactory = $resource('/projects.json/:id', { id: '@id' });
         var projects = projectsFactory.query();
-
         vm.projects = projects;
-
         vm.filteredProjects = projects;
 
+        var usersFactory = $resource('/users.json/:id', { id: '@id' });
+        var users = usersFactory.query();
+        vm.users = users;
+        vm.filteredUsers = users;
+
         vm.search = function(criteria){
+            if (!criteria) 
+                return;
+
             vm.filteredProjects = _.filter(vm.projects, function(proj)
                 {
                     return projectContains(proj, criteria);
+                });
+
+            vm.filteredUsers = _.filter(vm.users, function(user)
+                {
+                    return userContains(user, criteria);
                 });
         }
     }
