2883ec9e792d3d1296d308d91fc2cf9528f31411
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2014-10-25 08:46:20 +0200

    user-details

---
+++ app/assets/javascripts/directives/cut.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    function cut() {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+
+            return value + (tail || ' …');
+        };
+    }
+    
+    angular
+        .module('app')
+        .filter('cut', cut);
+        
+})();
