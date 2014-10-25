21c8c772cd74ad66ec1292d894390f9dd5d2c86d
Piotr Jaworski <piotr.jaworski@live.com>
2014-10-25 13:00:08 +0200

    Added skills to users and projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20141025091421) do
+ActiveRecord::Schema.define(version: 20141025105908) do
 
   create_table "applies", force: true do |t|
     t.datetime "created_at"
@@ -33,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141025091421) do
     t.datetime "updated_at"
     t.string   "name"
     t.integer  "apply_id"
+    t.text     "skills"
   end
 
   create_table "projects_applies", id: false, force: true do |t|
@@ -56,31 +56,7 @@ ActiveRecord::Schema.define(version: 20141025091421) do
     t.integer "user_id"
   end
 
-<<<<<<< HEAD
 # Could not dump table "users" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
-=======
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "role",                   default: 2
-    t.integer  "project_id"
-    t.string   "username"
-    t.text     "info"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> 6883a098c8c18033916ccac20ae6fcdeb030f18e
 
 end
