e1b694342895ee186c4d18ca1e754f99ed2ac3d3
bavaria <bavaria95@gmail.com>
2014-10-25 13:07:15 +0200

    changed profiles, editable user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,20 @@ class UsersController < ApplicationController
       format.json
     end
   end
+
+  def update
+  	@user = current_user
+    @user.update_attributes(user_params)
+    if @user.save
+      redirect_to root_path
+      flash[:success] = "Project has been updated!"
+    else
+      redirect_to root_path
+      flash[:danger] = "An error has accured, didn't you fill all fields?"
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:info)
+  end
 end
