a42657df7b89528038b9f68325f76997a47d37bb
bavaria <bavaria95@gmail.com>
2014-10-26 07:19:13 +0100

    needConfirmation

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -36,14 +36,24 @@ module Api
     end
 
     def needConfirmation
-      @user_find = current_user
+      @my_project_id = current_user.project_id
+      @users_waiting_for_accept = []
+      if @my_project_id
+        @users = User.all 
+        @users.each do |user|
+          if (user.is_confirmed == false) and (user.project_id == my_project_id)
+            @users_waiting_for_accept << user
+          end
+        end
+
+      render json: @users_waiting_for_accept
 
       # @user_find.project_id = nil
       # @user_find.is_confirmed = nil
       
       # @user_find.save!
 
-      render json: { status: "ok" }, status: 200
+      # render json: { status: "ok" }, status: 200
     end
   end
 end
