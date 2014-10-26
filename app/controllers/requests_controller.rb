e04cc138fdc00631a7c7ea5012ba9ad177d84255
Piotr Jaworski <piotr.jaworski@live.com>
2014-10-26 02:51:49 +0100

    Small changes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,7 +3,7 @@ class RequestsController < ApplicationController
   def accept
     @applier = User.find(params[:applier_id])
     @applier.update_attributes(is_confirmed: true)
-    @team_size = @applier.project.team_size
+    @team_size = @applier.project.team_size.to_i
     @applier.project.update_attributes(team_size: @team_size + 1)
     redirect_to root_path
     flash[:success] = "Have have added new hackmate!"
